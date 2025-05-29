2e534caa193deb01325a76924ba2f22a5d600502
Vishnu Rajeevan <github@vishnu.email>
2025-05-28 20:53:12 -0400

    only sync wishlist to tracker if its not in a user list

--- server/connectors/tracker/api/src/main/kotlin/com/vishnurajeevan/libroabs/connector/TrackerConnector.kt
+++ server/connectors/tracker/api/src/main/kotlin/com/vishnurajeevan/libroabs/connector/TrackerConnector.kt
@@ -4,6 +4,7 @@ interface TrackerConnector {
   suspend fun login()
   suspend fun getWantedBooks(): List<ConnectorBook>
   suspend fun getOwnedBooks(): List<ConnectorBook>
+  suspend fun getReadBooks(): List<ConnectorBook>
   suspend fun getEditions(isbn13s: List<String>): List<ConnectorBook>
   suspend fun createEdition(book: ConnectorBook): ConnectorBook?
   suspend fun markWanted(book: ConnectorBook)
