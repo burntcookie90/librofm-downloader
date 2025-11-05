8273d995354cf09125ab2cf9391735739ccd0656
Vishnu Rajeevan <github@vishnu.email>
2025-11-05 09:41:44 -0500

    Revert "attempt to pull down all pages for a users library"

--- server/src/main/kotlin/com/vishnurajeevan/libroabs/libro/LibraryMetadata.kt
+++ server/src/main/kotlin/com/vishnurajeevan/libroabs/libro/LibraryMetadata.kt
@@ -4,8 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class LibraryMetadata(
-  val page: Int,
-  @Suppress("PropertyName") val total_pages: Int,
   val audiobooks: List<Book> = emptyList(),
   val error: String? = null
 )
