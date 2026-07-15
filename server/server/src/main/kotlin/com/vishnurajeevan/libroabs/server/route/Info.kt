d1bd02dc7a6202fbec6ba754228602877fe6644e
Vishnu Rajeevan <github@vishnu.email>
2026-07-15 10:29:30 -0400

    dry up server route setup

--- server/server/src/main/kotlin/com/vishnurajeevan/libroabs/server/route/Info.kt
+++ server/server/src/main/kotlin/com/vishnurajeevan/libroabs/server/route/Info.kt
@@ -1,6 +1,24 @@
 package com.vishnurajeevan.libroabs.server.route
 
-import io.ktor.resources.Resource
+import com.vishnurajeevan.libroabs.models.server.ServerInfo
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ClassKey
+import dev.zacsweers.metro.ContributesBinding
+import dev.zacsweers.metro.ContributesIntoMap
+import dev.zacsweers.metro.Inject
+import io.ktor.resources.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 @Resource("/info")
 class Info
+
+@Inject
+@ContributesIntoMap(scope = AppScope::class)
+@ClassKey(Info::class)
+class InfoRouteHandler(private val serverInfo: ServerInfo): RouteHandler<Info> {
+  context(routingContext: RoutingContext)
+  override suspend fun handle(route: Info) {
+    routingContext.call.respond(serverInfo)
+  }
+}
